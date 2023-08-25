363f3e7a7bcfb2ca7e0da17400766d853152f09f
gKits <gkitsikoudis@gmx.de>
2023-08-25 13:34:27 +0200

    feat: BTree type with insert and update functionality on its pages

--- btree/btree.go
+++ btree/btree.go
@@ -3,54 +3,129 @@ package btree
 import "fmt"
 
 const (
-	PAGE_SIZE = 4096
+	PAGE_SIZE uint16 = 4096
 )
 
 type BTree struct {
-	root uint64
+	root  uint64
+	get   func(ptr uint64) Page
+	alloc func(p Page) uint64
+	free  func(ptr uint64)
 }
 
-// Returns the page at the given pointer.
-func (bt *BTree) GetPage(ptr uint64) Page { return nil }
-
-// Recursively walks through the BTree and inserts the cell into the correct page and returns it.
-// Returns an error if the page is malformed or the cell does not fit the pages requirements like
-// key uniquenes, key placement or cell size.
-func (bt *BTree) Insert(p Page, c Cell) (newPage Page, err error) {
+func (bt *BTree) bTreeInsert(p Page, c Cell) (Page, error) {
 	i, exists, err := p.BinSearchKeyIdx(c.Key())
 	if err != nil {
-		return nil, fmt.Errorf("btree: could not insert cell into page: %e", err)
-	}
-	if exists {
-		return nil, fmt.Errorf("btree: could not insert cell into page, key must be unique")
+		return nil, err
+	} else if !exists {
+		return nil, fmt.Errorf("")
 	}
 
+	inserted := Page{}
 	switch p.Type() {
-	case INTERNAL_PAGE:
-		internalC, err := p.GetCell(i)
-		if err != nil {
-			return nil, fmt.Errorf("btree: could not get cell: %e", err)
-		}
 
-		childPtr, err := internalC.GetChildPtr()
-		if err != nil {
-			return nil, fmt.Errorf("btree: could not get child pointer from cell value: %e", err)
-		}
-
-		newPage, err = bt.Insert(bt.GetPage(childPtr), c)
+	case LEAF_PAGE:
+		inserted, err = p.InsertCell(i, c)
 		if err != nil {
 			return nil, err
 		}
 
-	case LEAF_PAGE:
-		newPage, err = p.InsertCell(i, c)
+	case INTERNAL_PAGE:
+		ptrCell, _ := p.GetCell(i)
+		childPtr, _ := ptrCell.GetChildPtr()
+		child := bt.get(childPtr)
+
+		inserted, err = bt.bTreeInsert(child, c)
 		if err != nil {
-			return nil, fmt.Errorf("btree: could not insert new cell into page: %e", err)
+			return nil, err
 		}
 
+		if inserted.Size() > PAGE_SIZE {
+			inserted, err = bt.splitChildPtr(i, p, inserted)
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			insertedPtr := bt.alloc(inserted)
+			inserted, err = p.UpdateInternalCell(i, insertedPtr)
+			if err != nil {
+				return nil, err
+			}
+		}
+		bt.free(childPtr)
+
 	default:
-		return nil, fmt.Errorf("btree: bad page type")
+
 	}
 
-	return newPage, nil
+	return inserted, nil
+}
+
+func (bt *BTree) bTreeUpdate(p Page, c Cell) (Page, error) {
+	i, exists, err := p.BinSearchKeyIdx(c.Key())
+	if err != nil {
+		return nil, err
+	} else if !exists {
+		return nil, fmt.Errorf("")
+	}
+
+	updated := Page{}
+	switch p.Type() {
+
+	case LEAF_PAGE:
+		updated, err = p.UpdateCell(i, c)
+		if err != nil {
+			return nil, fmt.Errorf("")
+		}
+
+	case INTERNAL_PAGE:
+		ptrCell, _ := p.GetCell(i)
+		childPtr, _ := ptrCell.GetChildPtr()
+		child := bt.get(childPtr)
+
+		updated, err = bt.bTreeUpdate(child, c)
+		if err != nil {
+			return nil, fmt.Errorf("")
+		}
+
+		updatedPtr := bt.alloc(updated)
+
+		updated, err = p.UpdateInternalCell(i, updatedPtr)
+		if err != nil {
+			return nil, fmt.Errorf("")
+		}
+		bt.free(childPtr)
+
+	default:
+
+	}
+
+	return updated, nil
+}
+
+func (bt BTree) splitChildPtr(i uint16, parent, child Page) (Page, error) {
+	l, r := child.Split()
+	lPtr := bt.alloc(l)
+	rPtr := bt.alloc(r)
+
+	split, err := parent.UpdateInternalCell(i, lPtr)
+	if err != nil {
+		return nil, fmt.Errorf("")
+	}
+
+	rCell, err := r.GetCell(0)
+	if err != nil {
+		return nil, fmt.Errorf("")
+	}
+	rCell, err = rCell.SetChildPtr(rPtr)
+	if err != nil {
+		return nil, fmt.Errorf("")
+	}
+
+	split, err = parent.InsertCell(i, rCell)
+	if err != nil {
+		return nil, fmt.Errorf("")
+	}
+
+	return split, nil
 }
