da0e432be27d08c6947597bd7f4c107a73950d59
gKits <gkitsikoudis@gmx.de>
2023-08-25 13:23:29 +0200

    style: change formating for more consistency

--- btree/cell.go
+++ btree/cell.go
@@ -14,12 +14,29 @@ const (
 
 type Cell []byte
 
-func (c Cell) Type() CellType { return CellType(c[0]) }
-func (c Cell) Size() uint16   { return 5 + c.kSize() + c.vSize() }
-func (c Cell) Key() []byte    { return c[5 : 5+c.kSize()] }
-func (c Cell) Val() []byte    { return c[5+c.kSize() : 5+c.kSize()+c.vSize()] }
-func (c Cell) kSize() uint16  { return binary.BigEndian.Uint16(c[1:3]) }
-func (c Cell) vSize() uint16  { return binary.BigEndian.Uint16(c[3:5]) }
+func (c Cell) Type() CellType {
+	return CellType(c[0])
+}
+
+func (c Cell) Size() uint16 {
+	return 5 + c.kSize() + c.vSize()
+}
+
+func (c Cell) Key() []byte {
+	return c[5 : 5+c.kSize()]
+}
+
+func (c Cell) Val() []byte {
+	return c[5+c.kSize() : 5+c.kSize()+c.vSize()]
+}
+
+func (c Cell) kSize() uint16 {
+	return binary.BigEndian.Uint16(c[1:3])
+}
+
+func (c Cell) vSize() uint16 {
+	return binary.BigEndian.Uint16(c[3:5])
+}
 
 // Returns a new cell with the value set to v.
 // Returns an error if the size of v doesn't equal vSize of original cell c.
