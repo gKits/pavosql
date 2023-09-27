a881e1cb3deccc27a42414e2a2c190215af1e000
gKits <gkitsikoudis@gmx.de>
2023-09-27 09:36:37 +0200

    test: unittest for leafNode decode method

--- backend/leafNode_test.go
+++ backend/leafNode_test.go
@@ -1,207 +1,78 @@
 package backend
 
 import (
-	"crypto/rand"
-	"slices"
+	"bytes"
 	"testing"
 )
 
-func randBytes() []byte {
-	b := make([]byte, 10)
-	rand.Read(b)
-	return b
-}
-
-func TestSize(t *testing.T) {
-
-}
-
-func TestDecode(t *testing.T) {
+func TestLeafNodeDecode(t *testing.T) {
 	cases := []struct {
-		name string
-		d    []byte
-		leaf LeafNode
-		err  error
+		name        string
+		input       []byte
+		expected    *leafNode
+		expectedErr error
 	}{
 		{
-			"wrong type number throws error",
-			[]byte{0, 0},
-			LeafNode{},
-			errNodeDecode,
-		},
-	}
-
-	for _, c := range cases {
-		t.Run("", func(t *testing.T) {
-			leaf := LeafNode{}
-			err := leaf.Decode(c.d)
-
-			if c.err != err {
-				t.Errorf("unexpected error result: %s != %s", c.err, err)
-			} else {
-
-			}
-		})
-	}
-
-}
-
-func TestEncodeDecodeSize(t *testing.T) {
-	val := []byte("test")
-
-	cases := []struct {
-		leaf LeafNode
-	}{
-		{
-			LeafNode{
-				[][]byte{val, val, val},
-				[][]byte{val, val, val},
+			name: "Successful decoding",
+			input: []byte{
+				0x00, 0x01, // 1 is representation of lfNode nodeType
+				0x00, 0x03, // nKeys is equal to 3
+				0x00, 0x04, 0x00, 0x04, 'k', 'e', 'y', '1', 'v', 'a', 'l', '1', // first entry
+				0x00, 0x04, 0x00, 0x04, 'k', 'e', 'y', '2', 'v', 'a', 'l', '2', // second entry
+				0x00, 0x04, 0x00, 0x04, 'k', 'e', 'y', '3', 'v', 'a', 'l', '3', // third entry
 			},
-		},
-	}
-
-	for _, c := range cases {
-		t.Run("", func(t *testing.T) {
-			d := c.leaf.Encode()
-			n := LeafNode{}
-			n.Decode(d)
-
-			nSize := n.Size()
-			if len(d) != nSize {
-				t.Errorf("sizes are not equal: %d != %d", len(d), nSize)
-			}
-
-			if !slices.EqualFunc(n.keys, c.leaf.keys, func(b1, b2 []byte) bool {
-				return string(b1) == string(b2)
-			}) {
-				t.Errorf("keys are not equal: %v != %v", c.leaf.keys, n.keys)
-			}
-
-			if !slices.EqualFunc(n.vals, c.leaf.vals, func(b1, b2 []byte) bool {
-				return string(b1) == string(b2)
-			}) {
-				t.Errorf("vals are not equal: %v != %v", c.leaf.vals, n.vals)
-			}
-		})
-	}
-}
-
-func TestSearch(t *testing.T) {
-	val := []byte("test")
-
-	keysEven := [][]byte{
-		[]byte("b"), []byte("d"), []byte("f"), []byte("h"),
-		[]byte("j"), []byte("l"), []byte("n"), []byte("p"),
-		[]byte("r"), []byte("t"), []byte("v"), []byte("x"),
-	}
-
-	keysOdd := [][]byte{
-		[]byte("b"), []byte("d"), []byte("f"), []byte("h"),
-		[]byte("j"), []byte("l"), []byte("n"), []byte("p"),
-		[]byte("r"), []byte("t"), []byte("v"),
-	}
-
-	valsEven := [][]byte{
-		val, val, val, val,
-		val, val, val, val,
-		val, val, val, val,
-	}
-
-	valsOdd := [][]byte{
-		val, val, val, val,
-		val, val, val, val,
-		val, val, val,
-	}
-
-	cases := []struct {
-		name   string
-		leaf   LeafNode
-		search []byte
-		idx    int
-		exists bool
-	}{
-		{
-			"Even numbers of keys + search key exists",
-			LeafNode{
-				keysEven,
-				valsEven,
+			expected: &leafNode{
+				keys: [][]byte{{'k', 'e', 'y', '1'}, {'k', 'e', 'y', '2'}, {'k', 'e', 'y', '3'}},
+				vals: [][]byte{{'v', 'a', 'l', '1'}, {'v', 'a', 'l', '2'}, {'v', 'a', 'l', '3'}},
 			},
-			[]byte("d"),
-			1,
-			true,
+			expectedErr: nil,
 		},
 		{
-			"Even numbers of keys + search key does not exist",
-			LeafNode{
-				keysEven,
-				valsEven,
+			name: "Failed decoding due to wrong nodeType bytes",
+			input: []byte{
+				0x00, 0x02, // 2 is not the representation of lfNode nodeType
+				0x00, 0x03,
+				0x00, 0x04, 0x00, 0x04, 'k', 'e', 'y', '1', 'v', 'a', 'l', '1',
+				0x00, 0x04, 0x00, 0x04, 'k', 'e', 'y', '2', 'v', 'a', 'l', '2',
+				0x00, 0x04, 0x00, 0x04, 'k', 'e', 'y', '3', 'v', 'a', 'l', '3',
 			},
-			[]byte("e"),
-			2,
-			false,
-		},
-		{
-			"Odd numbers of keys + search key exists",
-			LeafNode{
-				keysOdd,
-				valsOdd,
+			expected: &leafNode{
+				keys: [][]byte{},
+				vals: [][]byte{},
 			},
-			[]byte("p"),
-			7,
-			true,
-		},
-		{
-			"Odd numbers of keys + search key does not exist",
-			LeafNode{
-				keysOdd,
-				valsOdd,
-			},
-			[]byte("u"),
-			10,
-			false,
-		},
-		{
-			"key before first index",
-			LeafNode{
-				keysEven,
-				valsEven,
-			},
-			[]byte("a"),
-			0,
-			false,
-		},
-		{
-			"key after last index",
-			LeafNode{
-				keysOdd,
-				valsOdd,
-			},
-			[]byte("z"),
-			11,
-			false,
+			expectedErr: errNodeDecode,
 		},
 	}
 
 	for _, c := range cases {
 		t.Run(c.name, func(t *testing.T) {
-			idx, exists := c.leaf.Search(c.search)
+			ln := &leafNode{}
 
-			if c.idx != idx {
-				t.Errorf("result idx does not match expected: %d != %d", c.idx, idx)
+			err := ln.decode(c.input)
+
+			if err != c.expectedErr {
+				t.Errorf("Expected error %v, but got %v", c.expectedErr, err)
 			}
-			if c.exists != exists {
-				t.Errorf("different existing status expected: %v != %v", c.exists, exists)
+
+			if len(ln.keys) != len(c.expected.keys) {
+				t.Errorf("Expected %v keys, but got %v", len(c.expected.keys), len(ln.keys))
+
+				for i, exp := range c.expected.keys {
+					if !bytes.Equal(ln.keys[i], exp) {
+						t.Errorf("Expected key %v at index %v, but got %v", exp, i, ln.keys[i])
+					}
+				}
+			}
+
+			if len(ln.vals) != len(c.expected.vals) {
+				t.Errorf("Expected %v vals, but got %v", len(c.expected.vals), len(ln.vals))
+
+				for i, exp := range c.expected.vals {
+					if !bytes.Equal(ln.vals[i], exp) {
+						t.Errorf("Expected val %v at index %v, but got %v", exp, i, ln.vals[i])
+					}
+				}
 			}
 		})
 	}
 }
-
-func TestMerge(t *testing.T) {}
-
-func TestSplit(t *testing.T) {}
-
-func TestInsert(t *testing.T) {}
-
-func TestUpdate(t *testing.T) {}
-
-func TestDelete(t *testing.T) {}
