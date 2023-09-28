b5afe93e0ef42a91a48c9c8f479548037e5b6a32
gKits <gkitsikoudis@gmx.de>
2023-09-28 21:25:06 +0200

    fix: decode, encode, size and search methods

--- backend/pointerNode.go
+++ backend/pointerNode.go
@@ -20,12 +20,12 @@ func (pn *pointerNode) decode(d []byte) error {
 	off := uint16(4)
 	for i := uint16(0); i < nKeys; i++ {
 		kSize := binary.BigEndian.Uint16(d[off : off+2])
-		pn.keys = append(pn.keys, d[off+4:off+4+kSize])
+		pn.keys = append(pn.keys, d[off+2:off+2+kSize])
 
-		ptr := binary.BigEndian.Uint64(d[off+4+kSize : off+4+kSize+8])
+		ptr := binary.BigEndian.Uint64(d[off+2+kSize : off+2+kSize+8])
 		pn.ptrs = append(pn.ptrs, ptr)
 
-		off += 4 + kSize + 8
+		off += 2 + kSize + 8
 	}
 
 	return nil
@@ -44,13 +44,12 @@ func (pn pointerNode) total() int {
 func (pn pointerNode) encode() []byte {
 	var b []byte
 
-	binary.BigEndian.AppendUint16(b, uint16(ptrNode))
-	binary.BigEndian.AppendUint16(b, uint16(len(pn.keys)))
+	b = binary.BigEndian.AppendUint16(b, uint16(ptrNode))
+	b = binary.BigEndian.AppendUint16(b, uint16(len(pn.keys)))
 	for i, k := range pn.keys {
-		ptr := pn.ptrs[i]
-		binary.BigEndian.AppendUint16(b, uint16(len(k)))
+		b = binary.BigEndian.AppendUint16(b, uint16(len(k)))
 		b = append(b, k...)
-		binary.BigEndian.AppendUint64(b, ptr)
+		b = binary.BigEndian.AppendUint64(b, pn.ptrs[i])
 	}
 
 	return b
@@ -59,7 +58,7 @@ func (pn pointerNode) encode() []byte {
 func (pn pointerNode) size() int {
 	size := 4
 	for _, k := range pn.keys {
-		size += 4 + len(k) + 8
+		size += 2 + len(k) + 8
 	}
 	return size
 }
@@ -75,22 +74,24 @@ func (ln pointerNode) search(k []byte) (int, bool) {
 	l := 0
 	r := len(ln.keys)
 
-	var i int
-	var cmp int
-
-	for i = r / 2; l < r; i = (l + r) / 2 {
-		cmp = bytes.Compare(k, ln.keys[i])
-		if cmp < 0 {
-			r = i - 1
+	i := r / 2
+	for {
+		if cmp := bytes.Compare(k, ln.keys[i]); cmp < 0 {
+			r = i
 		} else if cmp > 0 {
 			l = i + 1
 		} else {
 			return i, true
 		}
+
+		i = (l + r) / 2
+		if l >= r {
+			break
+		}
 	}
 
-	if cmp > 1 {
-		i++
+	if i < len(ln.keys) && bytes.Equal(ln.keys[i], k) {
+		return i, true
 	}
 
 	return i, false
