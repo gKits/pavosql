06965de387fd7790bf2883ec9951214f3b42eb3f
georgios.kitsikoudis <georgios.kitsikoudis@csoc.de>
2025-07-04 23:28:11 +0200

    feat: add cli blueprint with cobra

---
+++ cmd/pavosql/cmd/root.go
@@ -0,0 +1,37 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/pavosql/pavosql/cmd/pavosql/cmd/serve"
+	"github.com/pavosql/pavosql/cmd/pavosql/cmd/version"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "pavosql",
+	Short: "A brief description of your application",
+	Long: `A longer description that spans multiple lines and likely contains
+examples and usage of using your application. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	rootCmd.AddCommand(version.Command())
+	rootCmd.AddCommand(serve.Command())
+}
---
+++ pkg/fmt/fmt_test.go
@@ -0,0 +1,38 @@
+package fmt_test
+
+import (
+	"io"
+	"testing"
+
+	"github.com/pavosql/pavosql/pkg/fmt"
+)
+
+func TestFormat(t *testing.T) {
+	tests := []struct {
+		name string // description of this test case
+		// Named input parameters for target function.
+		r       io.Reader
+		want    []byte
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, gotErr := fmt.Format(tt.r)
+			if gotErr != nil {
+				if !tt.wantErr {
+					t.Errorf("Format() failed: %v", gotErr)
+				}
+				return
+			}
+			if tt.wantErr {
+				t.Fatal("Format() succeeded unexpectedly")
+			}
+			// TODO: update the condition below to compare got with tt.want.
+			if true {
+				t.Errorf("Format() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
