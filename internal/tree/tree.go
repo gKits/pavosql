983da754892925b7b3230f48a6e055a03c8f57b7
gkits <gkitsikoudis@gmx.de>
2025-07-05 20:26:44 +0200

    feat(tree): start implementing Get functionality

--- internal/tree/tree.go
+++ internal/tree/tree.go
@@ -1,7 +1,15 @@
 package tree
 
+import (
+	"errors"
+	"fmt"
+
+	"github.com/pavosql/pavosql/internal/common"
+	"github.com/pavosql/pavosql/internal/tree/node"
+)
+
 type pager interface {
-	ReadPage(off uint64) ([]byte, error)
+	ReadPage(off uint64) ([common.PageSize]byte, error)
 	Commit() error
 	Rollback() error
 }
@@ -16,7 +24,32 @@ func New() *Tree {
 }
 
 func (t *Tree) Get(k []byte) ([]byte, error) {
-	return nil, nil
+	page, err := t.pager.ReadPage(t.root)
+	if err != nil {
+		return nil, err
+	}
+	cur := node.Node(page)
+
+	for {
+		i, exists := cur.Search(k)
+
+		switch cur.Type() {
+		case common.PointerPage:
+			ptr := cur.Pointer(i)
+			page, err = t.pager.ReadPage(ptr)
+			if err != nil {
+				return nil, fmt.Errorf("tree: failed to read page: %w", err)
+			}
+			cur = node.Node(page)
+		case common.LeafPage:
+			if !exists {
+				return nil, errors.New("key does not exists on leaf node")
+			}
+			return cur.Val(i), nil
+		default:
+			return nil, errors.New("invalid page type")
+		}
+	}
 }
 
 func (t *Tree) Set(k []byte, v []byte) error {
